9251d72d596adf394206f74153dd00c4b5cc2ca3
David Chambers <David.Chambers.05@gmail.com>
2011-03-18 21:49:24 -0700

    Wrapped code in self-invoking anonymous function.

--- base64.js
+++ base64.js
@@ -1,57 +1,59 @@
-var Base64 = {
-  characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+;(function (window) {
 
-  encode: function (string) {
-    var
-      a, b, b1, b2, b3, b4, c,
-      characters = Base64.characters,
-      i = 0,
-      result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-    do {
-      a = string.charCodeAt(i++) || 0;
-      b = string.charCodeAt(i++) || 0;
-      c = string.charCodeAt(i++) || 0;
+  window.Base64 = {
+    encode: function (string) {
+      var
+        a, b, b1, b2, b3, b4, c,
+        i = 0,
+        result = '';
 
-      b1 = (a >> 2) & 0x3F;
-      b2 = ((a & 0x3) << 4) | ((b >> 4) & 0xF);
-      b3 = ((b & 0xF) << 2) | ((c >> 6) & 0x3);
-      b4 = c & 0x3F;
+      do {
+        a = string.charCodeAt(i++) || 0;
+        b = string.charCodeAt(i++) || 0;
+        c = string.charCodeAt(i++) || 0;
 
-      if (!b) {
-        b3 = b4 = 64;
-      } else if (!c) {
-        b4 = 64;
-      }
+        b1 = (a >> 2) & 0x3F;
+        b2 = ((a & 0x3) << 4) | ((b >> 4) & 0xF);
+        b3 = ((b & 0xF) << 2) | ((c >> 6) & 0x3);
+        b4 = c & 0x3F;
 
-      result += Base64.characters.charAt(b1) + Base64.characters.charAt(b2) + Base64.characters.charAt(b3) + Base64.characters.charAt(b4);
+        if (!b) {
+          b3 = b4 = 64;
+        } else if (!c) {
+          b4 = 64;
+        }
 
-    } while (i < string.length);
+        result += characters.charAt(b1) + characters.charAt(b2) + characters.charAt(b3) + characters.charAt(b4);
 
-    return result;
-  },
+      } while (i < string.length);
 
-  decode: function (string) {
-    var
-      a, b, b1, b2, b3, b4, c,
-      characters = Base64.characters,
-      i = 0,
-      result = '';
+      return result;
+    },
 
-    do {
-      b1 = Base64.characters.indexOf(string.charAt(i++));
-      b2 = Base64.characters.indexOf(string.charAt(i++));
-      b3 = Base64.characters.indexOf(string.charAt(i++));
-      b4 = Base64.characters.indexOf(string.charAt(i++));
+    decode: function (string) {
+      var
+        a, b, b1, b2, b3, b4, c,
+        i = 0,
+        result = '';
 
-      a = ((b1 & 0x3F) << 2) | ((b2 >> 4) & 0x3);
-      b = ((b2 & 0xF) << 4) | ((b3 >> 2) & 0xF);
-      c = ((b3 & 0x3) << 6) | (b4 & 0x3F);
+      do {
+        b1 = characters.indexOf(string.charAt(i++));
+        b2 = characters.indexOf(string.charAt(i++));
+        b3 = characters.indexOf(string.charAt(i++));
+        b4 = characters.indexOf(string.charAt(i++));
 
-      result += String.fromCharCode(a) + (b?String.fromCharCode(b):'') + (c?String.fromCharCode(c):'');
+        a = ((b1 & 0x3F) << 2) | ((b2 >> 4) & 0x3);
+        b = ((b2 & 0xF) << 4) | ((b3 >> 2) & 0xF);
+        c = ((b3 & 0x3) << 6) | (b4 & 0x3F);
 
-    } while (i < string.length);
+        result += String.fromCharCode(a) + (b?String.fromCharCode(b):'') + (c?String.fromCharCode(c):'');
 
-    return result;
-  }
-};
+      } while (i < string.length);
+
+      return result;
+    }
+  };
+
+}(this));
